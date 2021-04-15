1c87d0a17e9389e3b783681362486754818f3e29
Brad <BradPatras@users.noreply.github.com>
2021-04-14 22:59:20 -0500

    1.2: formatting prints

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -7,6 +7,23 @@ oooooo
 
 fn main() {
     println!("Hey Rust?");
+
+    println!("Wow {} arguments", "Rust has");
+
+    println!("Woah {lang}, {adjective} moves", 
+             lang="Rust",
+             adjective="nice");
+    
+    println!("hmm formatting on the fly {} == {:b}", 10, 2);
+
+    // Prints 1 padded with 5 white spaces = "     1"
+    println!("{number:>width$}", number=1, width=6);
+    
+    // Prints 1 padded with 5 zeros = "000001"
+    println!("{number:>0width$}", number=1, width=6);
+
+    let pi = 3.141592;
+    println!("Pi is roughly {:.*}", 3, pi);
 }
 
 
